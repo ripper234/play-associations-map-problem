82e3bbeab31c7a6712163ff4068729d591a34833
Ron Gross <ron.gross@gmail.com>
2011-12-15 22:48:02 +0200

    More tests

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -4,9 +4,12 @@ import models.MapOfBars;
 import org.junit.Test;
 import play.test.UnitTest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class BasicTest extends UnitTest {
     @Test
-    public void listOfBars() {
+    public void listOfBars_relyOnAssociationsModule() {
         ListOfBars list = new ListOfBars();
         Bar bar = new Bar();
         bar.k = "k";
@@ -16,12 +19,36 @@ public class BasicTest extends UnitTest {
     }
 
     @Test
-    public void mapOfBars() {
+    public void listOfBars_manual() {
+        ListOfBars list = new ListOfBars();
+        Bar bar = new Bar();
+        bar.k = "k";
+        bar.v = "v";
+        list.bars = new ArrayList<Bar>();
+        list.bars.add(bar);
+        bar.list = list;
+        list.save();
+    }
+
+    @Test
+    public void mapOfBars_relyOnAssociationsModule() {
         MapOfBars map = new MapOfBars();
         Bar bar = new Bar();
         bar.k = "k";
         bar.v = "v";
         map.bars.put("k", bar);
+        map.save();
+    }
+
+    @Test
+    public void mapOfBars_manual() {
+        MapOfBars map = new MapOfBars();
+        Bar bar = new Bar();
+        bar.k = "k";
+        bar.v = "v";
+        map.bars = new HashMap<String, Bar>();
+        map.bars.put("k", bar);
+        bar.map = map;
         map.save();
     }
 }
